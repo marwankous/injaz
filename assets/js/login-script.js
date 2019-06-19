0f9b6e3e6b5dfa2908d4db14cd97bf5014bd07c8
marwankous <o0xmarionx0o@gmail.com>
2019-06-19 21:07:36 +0100

    ...

---
+++ assets/js/login-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".signin").click(function(){
+    $(".overlay").css({"transform": "translate(-50%)", "transition-duration":"1.5s", "border-radius": "0.25rem 0rem 0rem 0.25rem"});
+    $( ".nt1" ).animate({opacity: 0 }, 500 );
+    $( ".nt2" ).animate({opacity: 1 }, 1000 );
+  });
+});
+$(document).ready(function(){
+  $(".signup").click(function(){
+    $(".overlay").css({"transform": "translate(50%)", "transition-duration":"1.5s","border-radius": "0rem 0.25rem 0.25rem 0rem"});
+    $( ".nt1" ).animate({opacity: 1 }, 1000 );
+    $( ".nt2" ).animate({opacity: 0 }, 500 );
+  });
+});
