633e0b7f4965c343a439f1071177dc1e896fdf79
marwankous <o0xmarionx0o@gmail.com>
2019-06-19 14:09:38 +0100

    ..

--- assets/js/index-js.js
+++ assets/js/index-js.js
@@ -6,7 +6,32 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     //Default data table
-    $('#default-datatable').DataTable();
+    $('#default-datatable').DataTable(
+        {
+            "decimal":        ",",
+            "emptyTable":     "aucune donnée disponible",
+            "info":           "Affichage de _START_ à _END_ sur _TOTAL_ entrées",
+            "infoEmpty":      "Affiche 0 à 0 de 0 entrées",
+            "infoFiltered":   "(filtré de _MAX_ Entrées totales)",
+            "infoPostFix":    "",
+            "thousands":      ".",
+            "lengthMenu":     "Afficher _MENU_ entrées",
+            "loadingRecords": "Chargement...",
+            "processing":     "En traitement...",
+            "search":         "Chercher:",
+            "zeroRecords":    "Aucun enregistrements correspondants trouvés",
+            "paginate": {
+                "first":      "Premier",
+                "last":       "Dernier",
+                "next":       "Suivant",
+                "previous":   "Précédent"
+            },
+            "aria": {
+                "sortAscending":  ": Activer pour trier la colonne par ordre croissant",
+                "sortDescending": ": Activer pour trier la colonne par ordre décroissant"
+            }
+        }
+    );
 
 
     var table = $('#example').DataTable( {
